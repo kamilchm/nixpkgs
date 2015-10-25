fc56e57a00934db9936705ec792c949d65f8c453
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2015-10-25 16:16:02 +0200

    sng: init at 1.0.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3024,6 +3024,10 @@ let
 
   snabb = callPackage ../tools/networking/snabb { } ;
 
+  sng = callPackage ../tools/graphics/sng {
+    libpng = libpng12;
+  };
+
   snort = callPackage ../applications/networking/ids/snort { };
 
   solr = callPackage ../servers/search/solr { };
