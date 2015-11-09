76adf31ac3cf0f1eb7d4f3911cb500b336e25f8a
Nikolay Amiantov <ab@fmap.me>
2015-11-09 17:12:18 +0300

    x86_enegry_perf_policy: init at kernel versions

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10065,6 +10065,8 @@ let
 
     virtualboxGuestAdditions = callPackage ../applications/virtualization/virtualbox/guest-additions { };
 
+    x86_energy_perf_policy = callPackage ../os-specific/linux/x86_energy_perf_policy { };
+
     zfs = callPackage ../os-specific/linux/zfs {
       configFile = "kernel";
       inherit kernel spl;
