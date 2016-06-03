9f9ebf0cf573f3616683499145580db6173a4a66
Kamil Chmielewski <kamil.chm@gmail.com>
2016-06-03 18:17:43 +0200

    skydns: extracted from goPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3322,7 +3322,7 @@ in
 
   skippy-xd = callPackage ../tools/X11/skippy-xd {};
 
-  skydns = goPackages.skydns.bin // { outputs = [ "bin" ]; };
+  skydns = (callPackage ../servers/skydns { }).bin;
 
   sipcalc = callPackage ../tools/networking/sipcalc { };
 
