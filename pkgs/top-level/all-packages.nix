9900217bea06da1f2e7183251c51915d58909929
Kamil Chmielewski <kamil.chm@gmail.com>
2016-06-04 13:25:34 +0200

    oauth2_proxy: extracted from goPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10011,7 +10011,7 @@ in
 
   nsq = callPackage ../servers/nsq { };
 
-  oauth2_proxy = goPackages.oauth2_proxy.bin // { outputs = [ "bin" ]; };
+  oauth2_proxy = callPackage ../servers/oauth2_proxy { };
 
   openpts = callPackage ../servers/openpts { };
 
