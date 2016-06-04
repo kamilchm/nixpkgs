bc64cd8abe6d843e4937646d65afac7d718f23f3
Kamil Chmielewski <kamil.chm@gmail.com>
2016-06-04 14:50:09 +0200

    goimports: extracted from goPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11118,7 +11118,7 @@ in
 
   godep = callPackage ../development/tools/godep { };
 
-  goimports = goPackages.tools.bin // { outputs = [ "bin" ]; };
+  goimports = callPackage ../development/tools/goimports { };
 
   gogoclient = callPackage ../os-specific/linux/gogoclient { };
 
