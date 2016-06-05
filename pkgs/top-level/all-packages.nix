770e018588dc1341082c8f929f55a1fea51cc38a
Kamil Chmielewski <kamil.chm@gmail.com>
2016-06-05 10:02:21 +0200

    syncthing012: extracted from goPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14235,7 +14235,7 @@ in
 
   syncthing = callPackage ../applications/networking/syncthing { };
 
-  syncthing012 = go15Packages.syncthing012.bin // { outputs = [ "bin" ]; };
+  syncthing012 = callPackage ../applications/networking/syncthing012 { };
 
   # linux only by now
   synergy = callPackage ../applications/misc/synergy { };
