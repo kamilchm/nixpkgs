ad166a6fe2a69af1e42b39e7182e279bf7350e26
Kamil Chmielewski <kamil.chm@gmail.com>
2016-06-05 16:41:51 +0200

    go2nix: extracted from goPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9606,7 +9606,7 @@ in
 
   goPackages = go15Packages;
 
-  go2nix = goPackages.go2nix.bin // { outputs = [ "bin" ]; };
+  go2nix = callPackage ../development/tools/go2nix { };
 
   ### DEVELOPMENT / LISP MODULES
 
