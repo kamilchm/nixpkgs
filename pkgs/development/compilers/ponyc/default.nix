17208697ce7b38826a6ece6d414e864cab0466d5
Kamil Chmielewski <kamil.chm@gmail.com>
2017-03-14 22:14:30 +0100

    ponyc: 0.11.0 -> 0.11.1

--- pkgs/development/compilers/ponyc/default.nix
+++ pkgs/development/compilers/ponyc/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation ( rec {
   name = "ponyc-${version}";
-  version = "0.11.0";
+  version = "0.11.1";
 
   src = fetchFromGitHub {
     owner = "ponylang";
     repo = "ponyc";
     rev = version;
-    sha256 = "0jjzc1711km53zkkxwy4r9ki9yxdx8mdjrp7g3mkr42p7q5h4w70";
+    sha256 = "0jinwp1q0di7vpcrwz7mvv9ij1wdhclllvjpgw6nzh20l2hgdiqs";
   };
 
   buildInputs = [ llvm makeWrapper which ];
